4f4a14a334dcede7b5379258f23f8900de270379
Rustem <61620276+Rustveider@users.noreply.github.com>
2023-05-29 21:16:24 +0300

    HomeWorkEx6

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,8 +1,14 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 
 public class HomeWork {
 
@@ -16,6 +22,18 @@ public class HomeWork {
                 .jsonPath();
         LinkedHashMap answer = responseForCheck.get("messages.find {it.message == 'And this is a second message'}");
         System.out.println(answer);
-
+    }
+    @Test
+    public void HomeWorkSix(){
+        Response response =
+                given().
+                        contentType(ContentType.URLENC).
+                        redirects().follow(false).
+                        expect().
+                        statusCode(301).
+                        when()
+                        .get("https://playground.learnqa.ru/api/long_redirect");
+        Header headerLocationValue = response.getHeaders().get("Location");
+        System.out.println(headerLocationValue);
     }
 }
